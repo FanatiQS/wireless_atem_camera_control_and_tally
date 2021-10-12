c7cc66147c4d34277b73880de519b5b177a33fdb
FanatiQS <fanatiqs.com@gmail.com>
2021-10-12 14:08:39 +0200

    Fixed include doc missmatch

--- test/client.c
+++ test/client.c
@@ -6,7 +6,7 @@
 #include <sys/socket.h> // recv, sendto, sockaddr_in, SOCK_DGRAM, AF_INET, socket
 #include <arpa/inet.h> // inet_addr, htons
 #include <sys/select.h> // fd_set, FD_ZERO, FD_SET, select
-#include <unistd.h> // sleep
+#include <unistd.h> // usleep
 
 #include "../src/atem.h"
 
