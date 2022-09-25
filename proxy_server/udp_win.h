adc5db5032484f58db72643dbfaa1570f2db7de6
FanatiQS <fanatiqs.com@gmail.com>
2022-09-25 17:08:02 +0200

    Update include doc

--- proxy_server/udp_win.h
+++ proxy_server/udp_win.h
@@ -3,9 +3,10 @@
 #define UDP_WIN_H
 
 #include <stdio.h> // fprintf, stderr
+#include <stdlib.h> // abort
 
 #define WIN32_LEAN_AND_MEAN
-#include <winsock2.h> // AF_INET, SOCK_DGRAM, socket, connect, bind, send, sendto, recv, recvfrom, sockaddr, sockaddr_in, INADDR_ANY, inet_addr, htons, fd_set, FD_ZERO, FD_SET, FD_ISSET, select, timeval, SOCKET, INVALID_SOCKET, inet_ntoa, timeval, WSAStartup, WSADATA, MAKEWORD, WSAGetLastError
+#include <winsock2.h> // AF_INET, SOCK_DGRAM, socket, connect, bind, send, sendto, recv, recvfrom, sockaddr, sockaddr_in, INADDR_ANY, inet_addr, htons, fd_set, FD_ZERO, FD_SET, FD_ISSET, select, timeval, SOCKET, INVALID_SOCKET, inet_ntoa, timeval, WSAStartup, WSADATA, MAKEWORD, WSAGetLastError, WSACleanup
 #pragma comment(lib, "ws2_32.lib")
 
 // Defines types not available in windows
