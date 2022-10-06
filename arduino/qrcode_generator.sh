a8fe4a335d1495cfde4dc76c161189003b22909a
FanatiQS <fanatiqs.com@gmail.com>
2022-10-06 16:01:34 +0200

    Uses local html for generation

--- arduino/qrcode_generator.sh
+++ arduino/qrcode_generator.sh
@@ -4,5 +4,5 @@ if [ $# -lt 2 ]; then
     exit 1;
 fi
 CHROME="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-URL="https://fanatiqs.github.io/wireless_atem_camera_control/arduino/qrcode_generator.html"
+URL="file://$PWD/$(dirname $0)/qrcode_generator.html"
 "$CHROME" --headless --disable-gpu --no-margins --print-to-pdf=$3 $URL?ssid=$1\&psk=$2
