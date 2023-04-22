912e8d6ce7f84b6cadd3a9a9fa878ae80766f835
FanatiQS <fanatiqs.com@gmail.com>
2023-04-22 14:06:48 +0200

    Inline functions from macros

--- firmware/led.h
+++ firmware/led.h
@@ -32,13 +32,9 @@
 
 
 
-// Gets tally bit masks from PGM and PVW states
-#define TALLY_SET_MASK(pgm, pvw) ((pgm * PIN_PGM_MASK) | (pvw * PIN_PVW_MASK))
-#define TALLY_CLR_MASK(pgm, pvw) ((!pgm * PIN_PGM_MASK) | (!pvw * PIN_PVW_MASK))
-
-
-
 #ifdef ESP8266
+
+#include <stdint.h> // uint32_t
 
 #include <eagle_soc.h> // GPIO_PIN_COUNT, GPIO_REG_WRITE, GPIO_PIN0_ADDRESS, GPIO_ENABLE_W1TS_ADDRESS, RTC_GPIO_ENABLE, GPIO_OUT_W1TC_DATA_MASK, GPIO_OUT_W1TS_ADDRESS, GPIO_OUT_W1TC_ADDRESS, WRITE_PERI_REG, RTC_GPIO_OUT
 
@@ -46,17 +42,17 @@
 #define RTC_PIN 16
 
 // Initializes GPIO or RTC pin
-#define LED_INIT(pin)\
-	do {\
-		if (pin < GPIO_PIN_COUNT) {\
-			GPIO_REG_WRITE(GPIO_PIN0_ADDRESS + pin * 4, 0);\
-			GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS, 1 << pin);\
-		}\
-		else if (pin == RTC_PIN) {\
-			WRITE_PERI_REG(RTC_GPIO_OUT, 0);\
-			WRITE_PERI_REG(RTC_GPIO_ENABLE, 1);\
-		}\
-	} while (0)
+inline void led_init(uint32_t pin) {
+	if (pin < GPIO_PIN_COUNT) {\
+		GPIO_REG_WRITE(GPIO_PIN0_ADDRESS + pin * 4, 0);\
+		GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS, 1 << pin);\
+	}\
+	else if (pin == RTC_PIN) {\
+		WRITE_PERI_REG(RTC_GPIO_OUT, 0);\
+		WRITE_PERI_REG(RTC_GPIO_ENABLE, 1);\
+	}\
+}
+#define LED_INIT(pin) led_init(pin)
 
 // Separates RTC pin from normal GPIO pins
 #define GPIO_MASK(mask) ((mask) & GPIO_OUT_W1TC_DATA_MASK)
@@ -64,18 +60,17 @@
 
 // Writes bit field to GPIO and/or RTC register
 // Pin masks are able to be eliminated by compiler when defined deterministically
-#define GPIO_WRITE(setPinMask, setValue, clearPinMask, clearValue)\
-	do {\
-		if (GPIO_MASK(setPinMask)) GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, GPIO_MASK(setValue));\
-		if (GPIO_MASK(clearPinMask)) GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, GPIO_MASK(clearValue));\
-		if (RTC_MASK(setPinMask | clearPinMask)) WRITE_PERI_REG(RTC_GPIO_OUT, RTC_MASK(setValue));\
-	} while (0)
+inline void gpio_write(uint32_t setPins, uint32_t setValue, uint32_t clearPins, uint32_t clearValue) {
+	if (GPIO_MASK(setPins)) GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, GPIO_MASK(setValue));\
+	if (GPIO_MASK(clearPins)) GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, GPIO_MASK(clearValue));\
+	if (RTC_MASK(setPins | clearPins)) WRITE_PERI_REG(RTC_GPIO_OUT, RTC_MASK(setValue));\
+}
 
 // Writes CONN (connection) state as bit fields to register
-#define LED_CONN_WRITE(setMask, clearMask) GPIO_WRITE(setMask, setMask, clearMask, clearMask)
+#define LED_CONN_WRITE(setMask, clearMask) gpio_write(setMask, setMask, clearMask, clearMask)
 
 // Writes tally states as bit fields to registers
-#define LED_TALLY_WRITE(setMask, clearMask) GPIO_WRITE(TALLY_MASK, setMask, TALLY_MASK, clearMask)
+#define LED_TALLY_WRITE(setMask, clearMask) gpio_write(TALLY_MASK, setMask, TALLY_MASK, clearMask)
 
 #else // ESP8266
 
@@ -83,9 +78,13 @@
 #error Platform does not support LED control
 #endif // PIN_PGM || PIN_PVW || PIN_CONN
 
-#endif
+#endif // ESP8266
 
 
+
+// Gets tally bit masks from PGM and PVW states
+#define TALLY_SET_MASK(pgm, pvw) ((pgm * PIN_PGM_MASK) | (pvw * PIN_PVW_MASK))
+#define TALLY_CLR_MASK(pgm, pvw) ((!pgm * PIN_PGM_MASK) | (!pvw * PIN_PVW_MASK))
 
 // Uses LED_TALLY_WRITE if LED_TALLY is not defined to convert states to bit fields
 #ifndef LED_TALLY
