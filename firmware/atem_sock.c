eab5e93e3fe560839f2b2e3fa0c2c79617b3072d
FanatiQS <fanatiqs.com@gmail.com>
2023-05-10 20:07:59 +0200

    Prints UDP connect error code

--- firmware/atem_sock.c
+++ firmware/atem_sock.c
@@ -231,7 +231,7 @@ static void atem_netif_poll(void* arg) {
 	sys_timeout(ATEM_TIMEOUT_MS, atem_timeout_callback, arg);
 }
 
-// Initializes UDP connection to ATEMs
+// Initializes UDP connection to ATEM
 struct udp_pcb* atem_init(uint32_t addr, uint8_t dest) {
 	// Sets camera id to serve
 	DEBUG_PRINTF("Filtering for camera ID: %d\n", dest);
@@ -252,8 +252,9 @@ struct udp_pcb* atem_init(uint32_t addr, uint8_t dest) {
 
 	// Connects to ATEM switcher
 	DEBUG_PRINTF("Connecting to ATEM at: " IP_FMT "\n", IP_VALUE(addr));
-	if (udp_connect(pcb, &(const ip_addr_t)IPADDR4_INIT(addr), ATEM_PORT) != ERR_OK) {
-		DEBUG_PRINTF("Failed to connect to ATEM UDP IP\n");
+	err_t err = udp_connect(pcb, &(const ip_addr_t)IPADDR4_INIT(addr), ATEM_PORT);
+	if (err != ERR_OK) {
+		DEBUG_PRINTF("Failed to connect to ATEM UDP IP: %d\n", err);
 		udp_remove(pcb);
 		return NULL;
 	}
