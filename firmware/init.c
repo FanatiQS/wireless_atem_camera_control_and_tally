9c226ad29d6cd0a745d9c274e21314f1e63bb67d
FanatiQS <fanatiqs.com@gmail.com>
2023-05-25 16:31:50 +0200

    Fixed unstabile soft ap without debug

--- firmware/init.c
+++ firmware/init.c
@@ -128,10 +128,10 @@ static void _atem_init() {
 		(int)sizeof(stationConfig.ssid), stationConfig.ssid,
 		(int)sizeof(stationConfig.password), stationConfig.password
 	);
+#endif // DEBUG
 
 	// Disables station scanning when soft ap is used to fix soft ap stability issue
 	wifi_set_event_handler_cb(network_callback);
-#endif // DEBUG
 
 	// Starts connecting to WiFi station
 	if (!wifi_station_connect()) {
