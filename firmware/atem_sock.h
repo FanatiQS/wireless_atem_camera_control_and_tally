d18c27d4205ba299784a47eed696bd41b25e48c7
FanatiQS <fanatiqs.com@gmail.com>
2023-09-20 18:00:40 +0200

    Add missing variable used by http_respond

--- firmware/atem_sock.h
+++ firmware/atem_sock.h
@@ -2,12 +2,17 @@
 #ifndef ATEM_SOCK_H
 #define ATEM_SOCK_H
 
-#include <stdint.h> // uint32_t, uint8_t
+#include <stdint.h> // uint8_t, uint32_t
 
 #include <lwip/udp.h> // struct udp_pcb
 
+#include "../src/atem.h" // struct atem_t
+
 // String representing the current ATEM connection status
 extern const char* atem_state;
+
+// The connection context to the ATEM switcher
+extern struct atem_t atem;
 
 // Initializes ATEM UDP client
 struct udp_pcb* atem_init(uint32_t addr, uint8_t dest);
