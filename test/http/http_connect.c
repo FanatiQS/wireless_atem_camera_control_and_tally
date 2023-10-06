5dc487d4a1277a8234ff1458f4f166668291239f
FanatiQS <fanatiqs.com@gmail.com>
2023-10-06 19:57:28 +0200

    Improved http connect tests

--- test/http/http_connect.c
+++ test/http/http_connect.c
@@ -1,7 +1,10 @@
 #include <stdio.h> // printf
 #include <stdlib.h> // getenv, atio
+#include <errno.h> // errno, ECONNRESET
+#include <assert.h> // assert
 
 #include <unistd.h> // usleep
+#include <sys/socket.h> // recv
 
 #include "../utils/http_sock.h" // http_socket_create, http_socket_send, http_socket_recv_len, http_socket_close, http_socket_recv_flush
 #include "../utils/runner.h" // RUN_TEST
@@ -13,10 +16,24 @@ int main(void) {
 
 	// Ensure there are no memory leaks for normal HTTP requests
 	RUN_TEST(
-		printf("Test basic requests\n");
+		printf("Test valid requests\n");
 		for (int i = 1; i <= iters; i++) {
 			int sock = http_socket_create();
 			http_socket_send(sock, "GET / HTTP/1.0\r\n\r\n");
+			http_socket_recv_cmp_status_line(sock, 200);
+			while (http_socket_recv_len(sock));
+			http_socket_close(sock);
+			if (!(i % 100)) printf("%d\n", i);
+		}
+	);
+
+	// Ensures there are no memory leaks for invalid HTTP request
+	RUN_TEST(
+		printf("Test invalid requests\n");
+		for (int i = 1; i <= iters; i++) {
+			int sock = http_socket_create();
+			http_socket_send(sock, "GEF / HTTP/1.0\r\n\r\n");
+			http_socket_recv_cmp_status_line(sock, 405);
 			while (http_socket_recv_len(sock));
 			http_socket_close(sock);
 			if (!(i % 100)) printf("%d\n", i);
@@ -25,11 +42,12 @@ int main(void) {
 
 	// Ensures there are no memory leaks when sending after server close
 	RUN_TEST(
-		printf("Test write after basic request\n");
+		printf("Test write after valid request\n");
 		for (int i = 1; i <= iters; i++) {
 			int sock = http_socket_create();
 			http_socket_send(sock, "GET / HTTP/1.0\r\n\r\n");
 			http_socket_send(sock, "X");
+			http_socket_recv_cmp_status_line(sock, 200);
 			while (http_socket_recv_len(sock));
 			http_socket_close(sock);
 			if (!(i % 100)) printf("%d\n", i);
@@ -39,12 +57,16 @@ int main(void) {
 	// @todo memory leak in arduino esp8266 lwip2 (not lwip1.4) causing oom (might be in rtos sdk too?)
 #if 0
 	RUN_TEST(
-		printf("Test write after invalid basic request\n");
+		printf("Test write after invalid request\n");
 		for (int i = 1; i <= iters; i++) {
 			int sock = http_socket_create();
 			http_socket_send(sock, "GEF / HTTP/1.1\r\n\r\n");
 			http_socket_send(sock, "X");
+			http_socket_recv_cmp_status_line(sock, 405);
 			http_socket_recv_flush(sock);
+			char buf[1024];
+			assert(recv(sock, buf, sizeof(buf), 0) == -1);
+			assert(errno == ECONNRESET);
 			http_socket_close(sock);
 			if (!(i % 100)) printf("%d\n", i);
 		}
