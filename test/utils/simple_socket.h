7a286fa1b43ba04690f2553bfcfa73c381c2048c
FanatiQS <fanatiqs.com@gmail.com>
2023-10-27 15:46:26 +0200

    Update include guard name

--- test/utils/simple_socket.h
+++ test/utils/simple_socket.h
@@ -1,6 +1,6 @@
 // Include guard
-#ifndef TEST_SOCKET_H
-#define TEST_SOCKET_H
+#ifndef SIMPLE_SOCKET_H
+#define SIMPLE_SOCKET_H
 
 #include <stddef.h> // size_t
 #include <stdbool.h> // bool
@@ -13,4 +13,4 @@ size_t simple_socket_recv(int sock, void* buf, size_t size);
 bool simple_socket_recv_error(int sock, int err, void* buf, size_t* len);
 int simple_socket_select(int sock, int timeout);
 
-#endif // TEST_SOCKET_H
+#endif // SIMPLE_SOCKET_H
