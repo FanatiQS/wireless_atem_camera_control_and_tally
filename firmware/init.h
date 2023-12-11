bbe991fda51239a84088e14f5f9ef6fbc6bd3ca4
FanatiQS <fanatiqs.com@gmail.com>
2023-12-11 11:19:22 +0100

    Change dev version

--- firmware/init.h
+++ firmware/init.h
@@ -3,7 +3,7 @@
 #define INIT_H
 
 // Firmware version
-#define FIRMWARE_VERSION_STRING "0.8.0-dev"
+#define FIRMWARE_VERSION_STRING "0.7.1-dev"
 
 // Makes it work with C++ (Arduino)
 #ifdef __cplusplus
