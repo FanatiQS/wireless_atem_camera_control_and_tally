0a985b8e36f2452fcd54b85fcae678609e26ca25
FanatiQS <fanatiqs.com@gmail.com>
2023-12-16 07:13:51 +0100

    Update comment about platforms

--- tools/shims/timespec_get/time.h
+++ tools/shims/timespec_get/time.h
@@ -8,7 +8,10 @@
 // Includes standard header file
 #include_next <time.h> // struct timespec, clock_gettime, CLOCK_REALTIME
 
-// Shim timespec_get for non-C11 platforms with POSIX.1-2001 support like MacOS 10.14 and earlier
+/*
+ * Shims timespec_get for non-C11 platforms that has POSIX.1-2001 support.
+ * This includes MacOS before 10.15 and Linux with glibc older than 2.16.
+ */
 #define TIME_UTC 0
 #define timespec_get(ts, base) clock_gettime(CLOCK_REALTIME, ts)
 
