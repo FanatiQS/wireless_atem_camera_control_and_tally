6c5162fecfe4008fcbe37a698163ca18ffcd546a
FanatiQS <fanatiqs.com@gmail.com>
2024-01-08 22:17:09 +0100

    Update comment

--- test/utils/runner.c
+++ test/utils/runner.c
@@ -17,7 +17,7 @@ static int runner_fails = 0;
 
 jmp_buf _runner_jmp;
 
-// Trap callback to exit test failing test and continue with next
+// Trap callback to exit failing test and continue with next
 static void runner_fail(int sig) {
 	// Prevents unused argument compiler warning
 	(void)sig;
