14b3a202a573cf4752ed331142675701c1961b56
FanatiQS <fanatiqs.com@gmail.com>
2024-01-15 21:32:15 +0100

    Add tests

--- test/atem_server/atem_server_close.c
+++ test/atem_server/atem_server_close.c
@@ -1,5 +1,37 @@
 #include "../utils/utils.h"
 
+// Connects to ATEM server and waits for server to start closing handshake
+uint16_t test_connect_and_drop(int sock) {
+	uint8_t packet[ATEM_MAX_PACKET_LEN];
+
+	// Connects to ATEM server
+	uint16_t sessionId = atem_handshake_connect(sock, 0x1234);
+
+	// Acknowledges ATEM state dump
+	do {
+		atem_acknowledge_keepalive(sock, packet);
+		atem_header_flags_get_verify(packet, ATEM_FLAG_ACKREQ, ATEM_FLAG_ACK);
+		atem_header_sessionid_get_verify(packet, sessionId);
+		atem_header_ackid_get_verify(packet, 0x0000);
+		atem_header_localid_get_verify(packet, 0x0000);
+	} while (atem_header_len_get(packet) > ATEM_LEN_HEADER);
+
+	// Does not acknowledge pings after state dump to force server initiated closing handshake
+	while (1) {
+		atem_socket_recv(sock, packet);
+		if (atem_header_flags_get(packet) & ATEM_FLAG_SYN) {
+			break;
+		}
+		atem_header_flags_get_verify(packet, ATEM_FLAG_ACKREQ, ATEM_FLAG_ACK | ATEM_FLAG_RETX);
+		atem_header_sessionid_get_verify(packet, sessionId);
+		atem_header_ackid_get_verify(packet, 0x0000);
+		atem_header_localid_get_verify(packet, 0x0000);
+	}
+	atem_handshake_sessionid_get_verify(packet, ATEM_OPCODE_CLOSING, false, sessionId);
+
+	return sessionId;
+}
+
 int main(void) {
 	// Ensures server closes session if not acknowledging opening handshake response
 	RUN_TEST() {
@@ -37,7 +69,7 @@ int main(void) {
 		atem_socket_close(sock);
 	}
 
-	// Ensures client sending closing request after server has already started a closing handshake is handled properly
+	// Ensures client and server starting closing handshake at the same time during opening handshake is handled properly
 	RUN_TEST() {
 		uint16_t clientSessionId = 0x1234;
 		int sock = atem_socket_create();
@@ -55,6 +87,41 @@ int main(void) {
 		atem_socket_norecv(sock);
 		atem_socket_close(sock);
 	}
+
+
+
+	// Ensures server closes session if not acknowledging pings
+	RUN_TEST() {
+		int sock = atem_socket_create();
+		uint16_t sessionId = test_connect_and_drop(sock);
+		atem_handshake_sessionid_send(sock, ATEM_OPCODE_CLOSED, false, sessionId);
+
+		atem_socket_norecv(sock);
+		atem_socket_close(sock);
+	}
+
+	// Ensures server resends closing request only once after client not acknowleding pings
+	RUN_TEST() {
+		int sock = atem_socket_create();
+		uint16_t sessionId = test_connect_and_drop(sock);
+		atem_handshake_sessionid_recv_verify(sock, ATEM_OPCODE_CLOSING, true, sessionId);
+
+		atem_socket_norecv(sock);
+		atem_socket_close(sock);
+	}
+
+	// Ensures client and server starting closing handshake at the same time after opening handshake is handled properly
+	RUN_TEST() {
+		int sock = atem_socket_create();
+		uint16_t sessionId = test_connect_and_drop(sock);
+		atem_handshake_sessionid_send(sock, ATEM_OPCODE_CLOSING, false, sessionId);
+		atem_handshake_sessionid_recv_verify(sock, ATEM_OPCODE_CLOSED, false, sessionId);
+
+		atem_socket_norecv(sock);
+		atem_socket_close(sock);
+	}
+
+
 
 	// Ensures client initiated closing request closes the session correctly
 	RUN_TEST() {
