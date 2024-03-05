34825f58579304f4359f6322cd030eb53898f488
FanatiQS <fanatiqs.com@gmail.com>
2024-03-05 17:03:21 +0100

    Refactor

--- firmware/flash.h
+++ firmware/flash.h
@@ -13,13 +13,15 @@ struct cache_wlan {
 	struct station_config station;
 	struct softap_config softap;
 };
-#define CACHE_WLAN struct cache_wlan
 
 // Linkers to ESP8266 specific wlan data structure in cache
 #define CACHE_SSID wlan.station.ssid
 #define CACHE_PSK wlan.station.password
 #define CACHE_NAME wlan.softap.ssid
+
 #endif // ESP8266
+
+
 
 // Masks for configuration flags
 #define CONF_FLAG_DHCP 0x01
@@ -32,26 +34,12 @@ struct config_t {
 	uint32_t atemAddr;
 	uint8_t flags;
 	uint8_t dest;
-#ifndef CACHE_SSID
-	char ssid[32];
-#define CACHE_SSID config.ssid
-#endif // CACHE_SSID
-#ifndef CACHE_PSK
-	char psk[64];
-#define CACHE_PSK config.psk
-#endif // CACHE_PSK
-#ifndef CACHE_NAME
-	char name[32];
-#define CACHE_NAME config.name
-#endif // CACHE_NAME
 };
 
 // Cached data for HTTP configuration
 struct cache_t {
 	struct config_t config;
-#ifdef CACHE_WLAN
-	CACHE_WLAN wlan;
-#endif // CONFIG_WLAN
+	struct cache_wlan wlan;
 };
 
 bool flash_config_read(struct config_t* conf);
