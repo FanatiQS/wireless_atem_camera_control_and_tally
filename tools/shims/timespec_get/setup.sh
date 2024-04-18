d354440f55322fa9c8bcefb2cf6ee69b42e1ddb0
FanatiQS <fanatiqs.com@gmail.com>
2024-04-18 04:26:01 +0200

    Add setup script

---
+++ tools/shims/timespec_get/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# Adds timespec_get shim through CPPFLAGS
+# Can be sourced to set environment variable `. path/to/script.sh`
+# Can be evaluated for comman substitution to inline environment variable content `$(path/to/script.sh) make`
+
+[ "$CPPFLAGS" != "" ] && CPPFLAGS="$CPPFLAGS "
+export CPPFLAGS=$CPPFLAGS-I$(cd $(dirname $BASH_SOURCE[0]); pwd)
+echo "CPPFLAGS=\"$CPPFLAGS\""
