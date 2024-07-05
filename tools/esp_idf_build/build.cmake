3c633f2d9188000fd4be5a3590ea2b5daa2e6b78
FanatiQS <fanatiqs.com@gmail.com>
2024-07-05 14:58:01 +0200

    Add build scripts for esp-idf

---
+++ tools/esp_idf_build/build.cmake
@@ -0,0 +1,10 @@
+set(SDKCONFIG ${CMAKE_BINARY_DIR}/sdkconfig)
+set(EXTRA_COMPONENT_DIRS ${CMAKE_CURRENT_LIST_DIR})
+
+message("----------------")
+message("CMAKE_BINARY_DIR: ${CMAKE_BINARY_DIR}")
+message("SDKCONFIG: ${SDKCONFIG}")
+message("EXTRA_COMPONENT_DIRS: ${EXTRA_COMPONENT_DIRS}")
+message("----------------")
+
+include($ENV{IDF_PATH}/tools/cmake/project.cmake)
