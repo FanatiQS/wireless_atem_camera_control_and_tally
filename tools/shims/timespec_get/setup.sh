7ca6d7d27a19df5eda9fc71b4623fd0c0841245b
FanatiQS <fanatiqs.com@gmail.com>
2024-09-10 16:57:49 +0200

    Fix spelling

--- tools/shims/timespec_get/setup.sh
+++ tools/shims/timespec_get/setup.sh
@@ -2,7 +2,7 @@
 
 # Adds timespec_get shim through CPPFLAGS
 # Can be sourced to set environment variable `. path/to/script.sh`
-# Can be evaluated for comman substitution to inline environment variable content `$(path/to/script.sh) make`
+# Can be evaluated for command substitution to inline environment variable content `$(path/to/script.sh) make`
 
 [ "$CPPFLAGS" != "" ] && CPPFLAGS="$CPPFLAGS "
 export CPPFLAGS=$CPPFLAGS-I$(cd $(dirname $BASH_SOURCE[0]); pwd)
