f0a0a87583791d8a61a83182807e9a844bd2e713
FanatiQS <fanatiqs.com@gmail.com>
2024-10-30 03:08:21 +0100

    Refactor

--- arduino/install.bat
+++ arduino/install.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
-cd %~dp0%
+cd %~dp0
 for %%i in ("%~dp0..") do set "root=%%~fi"
 echo #define LIB_PATH %root% > src\lib_path.h
-echo n | copy /-y "user_config.h" "waccat_config.h"
+if not exist "waccat_config.h" (
+	copy "user_config.h" "waccat_config.h"
+)
