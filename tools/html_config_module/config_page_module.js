68ff15d1d6514abdd30ad48170c0e10180f6a356
FanatiQS <fanatiqs.com@gmail.com>
2024-11-08 02:57:35 +0100

    Remove data-addr instead of hidden if not hidden

--- tools/html_config_module/config_page_module.js
+++ tools/html_config_module/config_page_module.js
@@ -24,11 +24,10 @@ const iframeLoad = new Promise((resolve) => iframe.contentWindow.addEventListene
  * Displayes the configuration page from a waccat device over the current DOM
  * @param {string} addr The ip address of the waccat device to display configuration for
  * @returns {Promise<boolean>} Indicates if configuration was submitted (true) or canceled (false)
-*/
+ */
 export async function displayConfigPage(addr) {
 	// Displays config page using address argument to capture and display configuration page from
 	iframe.setAttribute("data-addr", addr);
-	iframe.hidden = false;
 
 	// Returns promise resolved when configuration is submitted or canceled
 	await iframeLoad;
