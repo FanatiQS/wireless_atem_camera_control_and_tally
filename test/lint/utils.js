fce4e61a5c53cfa69d513d3c3242800871e6a45c
FanatiQS <fanatiqs.com@gmail.com>
2025-01-05 10:59:57 +0100

    Add jsdoc

--- test/lint/utils.js
+++ test/lint/utils.js
@@ -2,6 +2,11 @@
 
 import { execSync } from "node:child_process";
 
+/**
+ * Lists all files matching one of listed file extensions and is not ignored by any ignore files
+ * @param {string[]} suffixes
+ * @yields {string}
+ */
 export function* listFiles(suffixes) {
 	const flags = "--cached --others --exclude-standard";
 	const cmd = `git ls-files ${flags} ${suffixes.map((suffix) => `'*${suffix}'`).join(" ")}`;
