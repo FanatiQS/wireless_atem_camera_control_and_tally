8cc994c3780d5dac1d5bb7de8a43d6769ac47a11
FanatiQS <fanatiqs.com@gmail.com>
2025-01-07 12:52:02 +0100

    Refactor

--- test/lint/utils.js
+++ test/lint/utils.js
@@ -5,12 +5,12 @@ import { execSync } from "node:child_process";
 /**
  * Lists all files matching one of listed file extensions and is not ignored by any ignore files
  * @param {string[]} suffixes
- * @yields {string}
+ * @returns {string[]}
  */
-export function* listFiles(suffixes) {
+export function listFiles(suffixes) {
 	const flags = "--cached --others --exclude-standard";
 	const cmd = `git ls-files ${flags} ${suffixes.map((suffix) => `'*${suffix}'`).join(" ")}`;
-	for (const path of execSync(cmd).toString().split("\n").slice(0, -1)) {
-		yield `${import.meta.dirname}/../../${path}`;
-	}
+	return execSync(cmd).toString().split("\n").slice(0, -1).map((path) => {
+		return `${import.meta.dirname}/../../${path}`;
+	});
 }
