6282002bde0cdb4971bb67ad9872370736966551
FanatiQS <fanatiqs.com@gmail.com>
2025-04-25 19:12:50 +0200

    Add base client closing request test

--- test/atem_client/atem_client_close.c
+++ test/atem_client/atem_client_close.c
@@ -1,4 +1,5 @@
 #include <stdlib.h> // abort
+#include <stdint.h> // uint8_t, uint16_t
 
 #include "../utils/utils.h"
 
@@ -8,17 +9,17 @@ int main(void) {
 	// Ensures successful closing request closes the session
 	RUN_TEST() {
 		int sock = atem_socket_create();
-		uint16_t sessionId = atem_handshake_listen(sock, 0x0001);
+		uint16_t session_id = atem_handshake_listen(sock, 0x0001);
 
 		// Ensures client is connected by sending a ping
-		uint16_t remoteId = 0x0001;
-		atem_acknowledge_request_send(sock, sessionId, remoteId);
+		uint16_t remote_id = 0x0001;
+		atem_acknowledge_request_send(sock, session_id, remote_id);
 		uint8_t packet[ATEM_PACKET_LEN_MAX];
 		while (atem_acknowledge_keepalive(sock, packet));
-		atem_acknowledge_response_get_verify(packet, sessionId, remoteId);
+		atem_acknowledge_response_get_verify(packet, session_id, remote_id);
 
-		atem_handshake_sessionid_send(sock, ATEM_OPCODE_CLOSING, false, sessionId);
-		atem_handshake_sessionid_recv_verify(sock, ATEM_OPCODE_CLOSED, false, sessionId);
+		atem_handshake_sessionid_send(sock, ATEM_OPCODE_CLOSING, false, session_id);
+		atem_handshake_sessionid_recv_verify(sock, ATEM_OPCODE_CLOSED, false, session_id);
 		atem_socket_norecv(sock);
 		atem_socket_close(sock);
 	}
@@ -26,17 +27,17 @@ int main(void) {
 	// Ensures a retransmitted closing request is handled normally
 	RUN_TEST() {
 		int sock = atem_socket_create();
-		uint16_t sessionId = atem_handshake_listen(sock, 0x0001);
+		uint16_t session_id = atem_handshake_listen(sock, 0x0001);
 
 		// Ensures client is connected by sending a ping
-		uint16_t remoteId = 0x0001;
-		atem_acknowledge_request_send(sock, sessionId, remoteId);
+		uint16_t remote_id = 0x0001;
+		atem_acknowledge_request_send(sock, session_id, remote_id);
 		uint8_t packet[ATEM_PACKET_LEN_MAX];
 		while (atem_acknowledge_keepalive(sock, packet));
-		atem_acknowledge_response_get_verify(packet, sessionId, remoteId);
+		atem_acknowledge_response_get_verify(packet, session_id, remote_id);
 
-		atem_handshake_sessionid_send(sock, ATEM_OPCODE_CLOSING, true, sessionId);
-		atem_handshake_sessionid_recv_verify(sock, ATEM_OPCODE_CLOSED, false, sessionId);
+		atem_handshake_sessionid_send(sock, ATEM_OPCODE_CLOSING, true, session_id);
+		atem_handshake_sessionid_recv_verify(sock, ATEM_OPCODE_CLOSED, false, session_id);
 		atem_socket_norecv(sock);
 		atem_socket_close(sock);
 	}
@@ -44,21 +45,39 @@ int main(void) {
 	// Ensures client reconnects after being closed
 	RUN_TEST() {
 		int sock = atem_socket_create();
-		uint16_t sessionId = atem_handshake_listen(sock, 0x0001);
-		atem_handshake_close(sock, sessionId);
+		uint16_t session_id = atem_handshake_listen(sock, 0x0001);
+		atem_handshake_close(sock, session_id);
 		atem_socket_norecv(sock);
 		atem_socket_close(sock);
 
 		sock = atem_socket_create();
-		uint16_t reconnectSessionId = atem_handshake_start_server(sock);
-		if (sessionId == reconnectSessionId) {
+		uint16_t session_id_reconnect = atem_handshake_start_server(sock);
+		if (session_id == session_id_reconnect) {
 			fprintf(stderr,
 				"Expected reconnect with non server session id, but got previous id: %02x %02x\n",
-				sessionId, reconnectSessionId
+				session_id, session_id_reconnect
 			);
 			abort();
 		}
 		atem_socket_close(sock);
+	}
+
+
+
+	// Ensures not acknowledging packets causes client to close connection
+	RUN_TEST() {
+		// Client connects
+		int sock = atem_socket_create();
+		uint16_t session_id = atem_handshake_listen(sock, 0x0001);
+
+		// Waits from client to close
+		uint8_t packet[ATEM_PACKET_LEN_MAX];
+		do {
+			atem_socket_recv(sock, packet);
+		} while (atem_header_flags_get(packet) & ATEM_FLAG_ACKREQ);
+		atem_handshake_sessionid_get_verify(packet, ATEM_OPCODE_CLOSING, false, session_id);
+		atem_handshake_sessionid_send(sock, ATEM_OPCODE_CLOSED, false, session_id);
+		atem_socket_norecv(sock);
 	}
 
 	return runner_exit();
