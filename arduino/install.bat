460d89346eaddfcfa333142bdb7d06f4b5fedf8f
FanatiQS <fanatiqs.com@gmail.com>
2025-05-13 22:54:24 +0200

    Rename user_config.h to default_config.h

--- arduino/install.bat
+++ arduino/install.bat
@@ -4,5 +4,5 @@ cd %~dp0
 for %%i in ("%~dp0..") do set "root=%%~fi"
 echo #define LIB_PATH %root% > src\lib_path.h
 if not exist "waccat_config.h" (
-	copy "user_config.h" "waccat_config.h"
+	copy "default_config.h" "waccat_config.h"
 )
