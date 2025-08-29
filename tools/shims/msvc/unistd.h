82b442eabc5545df9688d91ebb331b2fa0594f86
FanatiQS <fanatiqs.com@gmail.com>
2025-08-29 20:56:53 +0200

    Use mingw sleep/usleep and fix backwards for msvc

--- tools/shims/msvc/unistd.h
+++ tools/shims/msvc/unistd.h
@@ -3,10 +3,27 @@
 #define UNISTD_H
 
 #include <winsock2.h> // closesocket
+
+#ifndef _MSC_VER
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wgnu-include-next"
+#include_next <unistd.h> // sleep, usleep
+#pragma GCC diagnostic pop
+#else
 #include <windows.h> // Sleep
 
-#define sleep(delay) Sleep(delay / 1000)
-#define usleep(delay) Sleep(delay * 1000)
+static inline unsigned sleep(unsigned seconds) {
+	Sleep(seconds * 1000);
+	return 0;
+}
+
+static inline int usleep(unsigned useconds) {
+	Sleep(useconds / 1000);
+	return 0;
+}
+#endif // _MSC_VER
+
+
 
 static inline int wsa_shim_close(int fd) {
 	errno = 0;
