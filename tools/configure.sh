c7bf4d65ef254fd812e494afa00050d9c8cb74c7
FanatiQS <fanatiqs.com@gmail.com>
2025-10-24 05:15:21 +0200

    Fix issues

--- tools/configure.sh
+++ tools/configure.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "Usage: $0 <ip-addr> key=value..."
-	exit
+	exit 1
 fi
 
 url=$1
 shift
 for arg in "$@"; do
 	echo "--data-urlencode \"$arg\""
-done | curl $url --silent --show-error --max-time 5 --config - | sed 's/.*<p>\([^<]*\).*/\1/'
+done | curl "$url" --silent --show-error --max-time 5 --config - | sed 's/.*<p>\([^<]*\).*/\1/'
