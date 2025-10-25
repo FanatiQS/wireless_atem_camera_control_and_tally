c06f5a372522d73570bdd0d6d0ea3e4f220dd57d
FanatiQS <fanatiqs.com@gmail.com>
2025-10-25 06:03:21 +0200

    Fix error

--- tools/shims/msvc/perror.c
+++ tools/shims/msvc/perror.c
@@ -2,7 +2,7 @@
 #include <errno.h> // errno
 #include <stdio.h> // perror, fprintf, stderr
 
-#include <winsock2.h> // WSAGetLastError
+#define WIN32_LEAN_AND_MEAN
 #include <winbase.h> // FormatMessageA, FORMAT_MESSAGE_FROM_SYSTEM, FORMAT_MESSAGE_IGNORE_INSERTS
 #include <winnt.h> // MAKELANGID, DWORD, LANG_ENGLISH, SUBLANG_ENGLISH_US
 
@@ -10,7 +10,7 @@
 
 // Replacement for perror function to also handle WSA errors stored as negative errno values
 void wsa_shim_perror(const char* msg) {
-	// Outputs normal error if set and clears errno
+	// Outputs normal errno error message
 	if (errno >= 0) {
 		perror(msg);
 		return;
@@ -18,10 +18,11 @@ void wsa_shim_perror(const char* msg) {
 
 	// Gets WSA error string
 	char buf[1024];
+	int err = -errno;
 	DWORD len = FormatMessageA(
 		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
 		NULL,
-		-errno,
+		err,
 		MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
 		buf,
 		sizeof(buf),
@@ -40,10 +41,10 @@ void wsa_shim_perror(const char* msg) {
 	// Outputs error code if unable to get error string
 	else {
 		if (msg == NULL || msg[0] == '\0') {
-			fprintf(stderr, "WSA error code %lu\n", err);
+			fprintf(stderr, "WSA error code %d\n", err);
 		}
 		else {
-			fprintf(stderr, "%s: WSA error code %lu\n", msg, err);
+			fprintf(stderr, "%s: WSA error code %d\n", msg, err);
 		}
 	}
 }
