0e3bb182a4f0236b7b4e2103d5fb8c51c8723545
FanatiQS <fanatiqs.com@gmail.com>
2025-11-16 12:01:05 +0100

    Fix renamed enums in newer idf

--- firmware/flash_esp_idf.c
+++ firmware/flash_esp_idf.c
@@ -6,7 +6,7 @@
 
 #include <esp_flash.h> // esp_flash_read, esp_flash_erase_region, esp_flash_write
 #include <esp_err.h> // esp_err_t, ESP_OK
-#include <esp_wifi.h> // esp_wifi_get_config, ESP_IF_WIFI_STA, ESP_IF_WIFI_AP, esp_wifi_set_config
+#include <esp_wifi.h> // esp_wifi_get_config, WIFI_IF_STA, WIFI_IF_AP, esp_wifi_set_config
 #include <esp_system.h> // esp_restart
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
 #include <spi_flash_mmap.h> // SPI_FLASH_SEC_SIZE
@@ -49,14 +49,14 @@ bool flash_cache_read(struct flash_cache* cache) {
 	esp_err_t err;
 
 	// Reads wlan configuration
-	err = esp_wifi_get_config(ESP_IF_WIFI_STA, &cache->wlan.station);
+	err = esp_wifi_get_config(WIFI_IF_STA, &cache->wlan.station);
 	if (err != ESP_OK) {
 		DEBUG_ERR_PRINTF("Failed to read station config: %x\n", err);
 		return false;
 	}
 
 	// Reads softap configuration for device name
-	err = esp_wifi_get_config(ESP_IF_WIFI_AP, &cache->wlan.softap);
+	err = esp_wifi_get_config(WIFI_IF_AP, &cache->wlan.softap);
 	if (err != ESP_OK) {
 		DEBUG_ERR_PRINTF("Failed to read softap config: %x\n", err);
 		return false;
@@ -71,12 +71,12 @@ void flash_cache_write(struct flash_cache* cache) {
 	esp_err_t err;
 
 	// Writes wlan configuration and device name to persistent storage
-	err = esp_wifi_set_config(ESP_IF_WIFI_STA, &cache->wlan.station);
+	err = esp_wifi_set_config(WIFI_IF_STA, &cache->wlan.station);
 	if (err != ESP_OK) {
 		DEBUG_ERR_PRINTF("Failed to write station config: %x\n", err);
 		return;
 	}
-	err = esp_wifi_set_config(ESP_IF_WIFI_AP, &cache->wlan.softap);
+	err = esp_wifi_set_config(WIFI_IF_AP, &cache->wlan.softap);
 	if (err != ESP_OK) {
 		DEBUG_ERR_PRINTF("Failed to write softap config: %x\n", err);
 		return;
