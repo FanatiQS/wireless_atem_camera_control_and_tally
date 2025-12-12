4d7c8d39bcb9b18c2e658289ab4a65022ceaefae
FanatiQS <fanatiqs.com@gmail.com>
2025-12-12 19:12:49 +0100

    atem_recv also parses the content

--- core/atem_posix.c
+++ core/atem_posix.c
@@ -10,9 +10,9 @@
 #include <unistd.h> // close
 #include <sys/types.h> // ssize_t
 
-#include "./atem.h" // struct atem, ATEM_PORT, atem_connection_open, ATEM_TIMEOUT_MS, ATEM_PACKET_LEN_MAX, enum atem_status, atem_parse, atem_cmd_available
+#include "./atem.h" // struct atem, ATEM_PORT, atem_connection_open, ATEM_TIMEOUT_MS, ATEM_PACKET_LEN_MAX, atem_parse, atem_cmd_available
 #include "./atem_protocol.h" // ATEM_LEN_HEADER
-#include "./atem_posix.h" // enum atem_posix_status, ATEM_POSIX_STATUS_ERROR_NETWORK, ATEM_POSIX_STATUS_DROPPED
+#include "./atem_posix.h" // enum atem_posix_status, ATEM_POSIX_STATUS_ERROR_NETWORK, ATEM_POSIX_STATUS_ERROR_PARSE, ATEM_POSIX_STATUS_DROPPED
 
 // Initializes ATEM communication by creating UDP socket for context
 bool atem_init(struct atem_posix_ctx* atem_ctx, in_addr_t addr) {
@@ -31,13 +31,18 @@ bool atem_init(struct atem_posix_ctx* atem_ctx, in_addr_t addr) {
 	}
 
 	// Connects UDP socket to only communicate with ATEM server
-	struct sockaddr_in sockaddr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(ATEM_PORT),
-		.sin_addr.s_addr = addr
+	union {
+		struct sockaddr_in inaddr;
+		struct sockaddr addr;
+	} sockaddr = {
+		.inaddr.sin_family = AF_INET,
+		.inaddr.sin_port = htons(ATEM_PORT),
+		.inaddr.sin_addr.s_addr = addr
 	};
-	if (connect(atem_ctx->sock, (const struct sockaddr*)&sockaddr, sizeof(sockaddr))) {
+	if (connect(atem_ctx->sock, &sockaddr.addr, sizeof(sockaddr))) {
+		int err = errno;
 		close(atem_ctx->sock);
+		errno = err;
 		return false;
 	}
 
@@ -62,11 +67,23 @@ bool atem_send(struct atem_posix_ctx* atem_ctx) {
 	return sent == atem->write_len;
 }
 
-// Reads next ATEM packet from server
-bool atem_recv(struct atem_posix_ctx* atem_ctx) {
+// Reads next ATEM packet from server and parses its content
+enum atem_posix_status atem_recv(struct atem_posix_ctx* atem_ctx) {
 	assert(atem_ctx != NULL);
-	errno = 0;
-	return recv(atem_ctx->sock, atem_ctx->atem.read_buf, ATEM_PACKET_LEN_MAX, 0) >= ATEM_LEN_HEADER;
+
+	ssize_t recved = recv(atem_ctx->sock, atem_ctx->atem.read_buf, sizeof(atem_ctx->atem.read_buf), 0);
+	assert(recved >= -1);
+	assert(recved <= (ssize_t)sizeof(atem_ctx->atem.read_buf));
+
+	if (recved >= ATEM_LEN_HEADER) {
+		return (enum atem_posix_status)atem_parse(&atem_ctx->atem);
+	}
+	else if (recved == -1) {
+		return ATEM_POSIX_STATUS_ERROR_NETWORK;
+	}
+	else {
+		return ATEM_POSIX_STATUS_ERROR_PARSE;
+	}
 }
 
 // Reads and parses ATEM packets until a packet that could contain a payload is received
@@ -88,17 +105,12 @@ enum atem_posix_status atem_poll(struct atem_posix_ctx* atem_ctx) {
 		return ATEM_POSIX_STATUS_DROPPED;
 	}
 
-	if (!atem_recv(atem_ctx)) {
-		return ATEM_POSIX_STATUS_ERROR_NETWORK;
-	}
-
 	// Parses and acknowledges received ATEM packet
-	enum atem_status status = atem_parse(&atem_ctx->atem);
-	if (!(status & 1)) {
+	enum atem_posix_status status = atem_recv(atem_ctx);
+	if (status > 0 && !(status & 1)) {
 		atem_send(atem_ctx);
 	}
-
-	return (enum atem_posix_status)status;
+	return status;
 }
 
 // Returns status codes or iterates through commands in ATEM packets
