51c2e4fe2920e2b79ce8d494e7f487b0d10d980b
FanatiQS <fanatiqs.com@gmail.com>
2025-12-12 19:24:51 +0100

    Add core posix tests

---
+++ test/core/core_posix.c
@@ -0,0 +1,56 @@
+#include <assert.h> // assert
+#include <errno.h> // errno, EFAULT
+#include <stdbool.h> // true, false
+#include <stdint.h> // uint8_t
+
+#include <arpa/inet.h> // htonl
+#include <netinet/in.h> // INADDR_LOOPBACK, in_addr_t
+#include <sys/socket.h> // socklen_t, struct sockaddr, getsockname, connect
+
+#include "../utils/utils.h"
+
+int main(void) {
+	// Ensures valid ip address argument is accepted
+	RUN_TEST() {
+		struct atem_posix_ctx posix_client;
+		assert(atem_init(&posix_client, INADDR_LOOPBACK) == true);
+	}
+
+	// Ensures invalid ip address argument is rejected with error code
+	RUN_TEST() {
+		struct atem_posix_ctx posix_client;
+		assert(atem_init(&posix_client, (in_addr_t)-1) == false);
+		assert(errno == EFAULT);
+	}
+
+	// Ensures too short payload fails to parse
+	RUN_TEST() {
+		// Initialises ATEM server
+		int server_sock = atem_socket_create();
+		simple_socket_listen(server_sock, ATEM_PORT);
+
+		// Initializes ATEM posix client
+		struct atem_posix_ctx posix_client;
+		assert(atem_init(&posix_client, htonl(INADDR_LOOPBACK)));
+
+		// Connects server to client
+		struct sockaddr addr;
+		socklen_t addrlen = sizeof(addr);
+		assert(getsockname(posix_client.sock, &addr, &addrlen) == 0);
+		assert(connect(server_sock, &addr, addrlen) == 0);
+
+		// Sends too short packet
+		uint8_t buf[ATEM_LEN_HEADER - 1] = {0};
+		simple_socket_send(server_sock, buf, sizeof(buf));
+
+		// Expects parse to fail
+		assert(atem_recv(&posix_client) == ATEM_POSIX_STATUS_ERROR_PARSE);
+	}
+
+	// Ensures timing out returns correct status enum
+	RUN_TEST() {
+		struct atem_posix_ctx posix_client;
+		assert(atem_init(&posix_client, htonl(INADDR_LOOPBACK)));
+		assert(atem_poll(&posix_client) == ATEM_POSIX_STATUS_DROPPED);
+	}
+}
