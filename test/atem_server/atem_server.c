bbf5932129bb18f1b226330e3eb5f73f7f0cb41b
FanatiQS <fanatiqs.com@gmail.com>
2026-01-04 12:13:21 +0100

    Add missing atem_server_commands

--- test/atem_server/atem_server.c
+++ test/atem_server/atem_server.c
@@ -5,10 +5,11 @@ void atem_server(void) {
 	atem_server_open();
 	atem_server_close();
 	atem_server_data();
+	atem_server_commands();
 	atem_server_cc();
 }
 
-// Runs all exteended ATEM server tests
+// Runs all extended ATEM server tests
 void atem_server_extended(void) {
 	atem_server_open_extended();
 	atem_server_data_extended();
